8bde7d73ff53514ac4c6341abcf8d0c40e7bc1e5
joseph0926 <joseph0926@naver.com>
2024-12-22 19:06:20 +0900

    [feature]: url searchparams 업데이트 context 정의

---
+++ apps/client/src/context/url.context.tsx
@@ -0,0 +1,78 @@
+import { createContext, useCallback, useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+
+type NewSearchParamsType = {
+  [key: string]: string;
+};
+type UpdateMode = "replace" | "update";
+
+type UrlContextProps = {
+  searchParams: URLSearchParams;
+  onUpdateSearchParams: (
+    payload: NewSearchParamsType,
+    mode?: UpdateMode
+  ) => void;
+};
+
+const UrlContext = createContext<UrlContextProps | null>(null);
+
+export const UrlContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  /**
+   * searchParams 업데이트 함수
+   * @param payload 새로운 searchParams
+   * @param mode "replace" | "update"
+   */
+  const onUpdateSearchParams = useCallback(
+    (payload: NewSearchParamsType, mode: UpdateMode = "replace") => {
+      // 빈 객체면 리턴
+      if (Object.keys(payload).length === 0) {
+        console.warn("[UrlContext] payload가 제공되지 않았습니다.");
+        return;
+      }
+
+      // 객체가 {[key: string]: string} 타입인지 체크
+      const isValidPayload = Object.entries(payload).every(
+        ([key, value]) => typeof key === "string" && typeof value === "string"
+      );
+      if (!isValidPayload) {
+        console.warn(
+          "[UrlContext] payload 타입은 { [key: string]: string } 이어야 합니다."
+        );
+        return;
+      }
+
+      // 현재 searchParams 객체로 변환
+      const currentParams = Object.fromEntries(searchParams.entries());
+
+      if (mode === "replace") {
+        setSearchParams(payload);
+      } else {
+        const updateParams = {
+          ...currentParams,
+          ...payload,
+        };
+        setSearchParams(updateParams);
+      }
+    },
+    [searchParams, setSearchParams]
+  );
+
+  const initialValue = useMemo(() => {
+    return {
+      searchParams,
+      onUpdateSearchParams,
+    };
+  }, [searchParams, onUpdateSearchParams]);
+
+  return (
+    <UrlContext.Provider value={initialValue}>{children}</UrlContext.Provider>
+  );
+};
+
+export default UrlContext;
---
+++ apps/client/src/hooks/use-url-context.ts
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import UrlContext from "../context/url.context";
+
+export const useUrlContext = () => {
+  const context = useContext(UrlContext);
+  if (!context) {
+    throw new Error(
+      "[UrlContext] useUrlContext은 반드시 UrlContextProvider 내부에서 사용되어야합니다."
+    );
+  }
+  return context;
+};
