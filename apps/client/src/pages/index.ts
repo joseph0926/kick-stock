05c1e8c78d69d762fea9b34ecd8fc15c06738c0e
김영훈 <joseph0926@naver.com>
2024-12-23 08:11:18 +0900

    [feature]: client 라우터 정의

---
+++ apps/client/src/pages/index.ts
@@ -0,0 +1,3 @@
+export * from "./layouts/root.layout";
+export * from "./home.page";
+export * from "./landing.page";
---
+++ apps/client/src/router/index.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+import { ROUTER } from "@/constants/router";
+import { HomePage, LandingPage, RootLayout } from "@/pages";
+
+export const router = createBrowserRouter([
+  {
+    path: ROUTER.HOME,
+    element: <RootLayout />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+  {
+    lazy: async () => {
+      const { AuthLayout } = await import("@/pages/layouts/auth.layout");
+      return { Component: AuthLayout };
+    },
+    children: [
+      {
+        path: ROUTER.SIGNIN,
+        lazy: async () => {
+          const { SignInPage } = await import("@/pages/sign-in.page");
+          return { Component: SignInPage };
+        },
+      },
+      {
+        path: ROUTER.SIGNUP,
+        lazy: async () => {
+          const { SignUpPage } = await import("@/pages/sign-up.page");
+          return { Component: SignUpPage };
+        },
+      },
+    ],
+  },
+  {
+    path: ROUTER.LANDING,
+    element: <LandingPage />,
+  },
+]);
