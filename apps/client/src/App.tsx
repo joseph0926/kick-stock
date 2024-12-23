f1c50fd7b81f899565a2f3308328c8e55c66fabf
김영훈 <joseph0926@naver.com>
2024-12-24 08:29:34 +0900

    [feature] 배경색 지정

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,11 @@
 import { RouterProvider } from "react-router";
 import { router } from "@/router";
+import { ThemeProvider } from "./context/theme.context";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
--- apps/client/src/pages/layouts/root.layout.tsx
+++ apps/client/src/pages/layouts/root.layout.tsx
@@ -1,5 +1,9 @@
 import { Outlet } from "react-router";
 
 export function RootLayout() {
-  return <Outlet />;
+  return (
+    <div className="w-full min-h-screen bg-background-transparent">
+      <Outlet />
+    </div>
+  );
 }
