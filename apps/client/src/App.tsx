ca7ec57102e41ebf9ae60db9aba6203f361095e0
김영훈 <joseph0926@naver.com>
2024-12-26 07:30:52 +0900

    [feature] client react-query 추가

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,17 @@
 import { RouterProvider } from "react-router";
 import { router } from "@/router";
 import { ThemeProvider } from "./context/theme.context";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/query-client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
