98f96d584ce29d863a15aee6907901f4e7e450db
김영훈 <joseph0926@naver.com>
2024-12-27 08:11:17 +0900

    [reafctor] logo & title 수정

--- apps/client/src/App.server.tsx
+++ apps/client/src/App.server.tsx
@@ -7,10 +7,10 @@ export default function App({ children }: { children: React.ReactNode }) {
     <html lang="ko">
       <head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <link rel="icon" type="image/svg+xml" href="/dist/logo.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/dist/assets/index.css"></link>
-        <title>StockMock</title>
+        <title>KickStock</title>
       </head>
       <body>
         <div id="root">{children}</div>
