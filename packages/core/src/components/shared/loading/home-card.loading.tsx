d1110ede47eccee449e497f7d93dd3d596b8cd6b
joseph0926 <joseph0926@naver.com>
2025-01-04 15:59:17 +0900

    [refactor] 로딩 개선

--- packages/core/src/components/shared/loading/home-card.loading.tsx
+++ packages/core/src/components/shared/loading/home-card.loading.tsx
@@ -2,7 +2,6 @@ import { CarouselItem } from "@kickstock/ui/src/components/ui/carousel";
 import { Link } from "react-router";
 import React from "react";
 import { Card } from "@kickstock/ui/src/components/ui/card";
-import { Skeleton } from "@kickstock/ui/src/components/ui/skeleton";
 
 export const HomeCardLoading = () => {
   const skeletonItems = React.useMemo(() => Array.from({ length: 5 }), []);
@@ -15,9 +14,7 @@ export const HomeCardLoading = () => {
           className="pl-1 sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
         >
           <Link to="#" className="p-1">
-            <Card className="flex flex-col items-center gap-4 p-2">
-              <Skeleton className="flex h-72 items-center gap-4 p-2 px-8" />
-            </Card>
+            <Card className="flex h-[274px] w-full flex-col items-center gap-4 p-2" />
           </Link>
         </CarouselItem>
       ))}
--- packages/core/src/components/shared/loading/home-chart.loading.tsx
+++ packages/core/src/components/shared/loading/home-chart.loading.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Skeleton } from "@kickstock/ui/src/components/ui/skeleton";
 
 export const HomeChartLoading = () => {
-  return <Skeleton className="min-h-[200px] w-full" />;
+  return <Skeleton className="min-h-[200px] w-[250px]" />;
 };
